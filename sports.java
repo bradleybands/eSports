38429a6ca3147bdb636f703c14504c60c1eb919d
Mike Ofori <61565391+monk63@users.noreply.github.com>
2020-12-02 10:38:36 +0000

    Update sports.java
    Co-Authored-By: Amenoreys <75338353+Amenoreys@users.noreply.github.com>
    Co-Authored-By: bradleybands <75338005+bradleybands@users.noreply.github.com>

--- sports.java
+++ sports.java
@@ -4,6 +4,34 @@
 public class sports{
 
     //Variables 
+    public static void main(String[] args){
+        
+       
+        String original="momm";
+        original= original.replace(" ", "");
+
+        String reverse="";
+        for (int i=original.length()-1;i>=0;i--){
+            reverse+=original.charAt(i);
+            System.out.println(reverse);
+            
+        }
+        boolean palindrome=true;
+        for(int i=0;i<original.length();i++){
+            if(original.charAt(i)!=reverse.charAt(i)){
+                palindrome=false;
+            }
+            
+
+        }
+        if(palindrome){
+            System.out.println("PALINDROME");
+        }else{
+            System.out.println("Not a palindrome");
+        }
+
+
+    }
 
 
 }
